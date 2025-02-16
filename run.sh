b837924027591b17ffd8363527ce1b8d36081a75
Peter <peter@kokosnoot>
2025-02-16 23:00:59 +0100

    IT-173 run op kokosnoot

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker stop thuis
 docker rm thuis
-docker run --publish 8088:8088 --name thuis thuis
+docker run --publish 8088:8088 --volume /opt/thuis/envdb.json:/usr/src/app/envdb.json --name thuis thuis
