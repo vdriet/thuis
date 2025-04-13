d00d23bea00c208ece10983a145b726c50f03bf8
Peter <peter@kokosnoot>
2025-04-13 11:18:07 +0200

    etc/hosts van host

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ docker run \
         --detach \
         --publish 8088:8088 \
         --volume /opt/thuis/envdb.json:/usr/src/app/envdb.json \
+        --volume /etc/hosts:/etc/hosts \
         --name thuis \
         --env-file env.list \
         thuis
