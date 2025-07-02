67ecb908bffcb43b4eea866c1257dbd6baf51a2e
Peter <peter@kokosnoot>
2025-07-02 20:07:04 +0200

    restart na crash

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ docker stop thuis
 docker rm thuis
 docker run \
         --detach \
+        --restart unless-stopped \
         --publish 8088:8088 \
         --volume /opt/thuis/envdb.json:/usr/src/app/envdb.json \
         --volume /opt/thuis/zonnesterkte.json:/usr/src/app/zonnesterkte.json \
