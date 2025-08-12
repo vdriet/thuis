03eaef2d45a880377172152596d05dc468e15c6f
Peter <peter@kokosnoot>
2025-08-12 22:07:09 +0200

    IT-333 work-around voor resolve van host

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ docker stop thuis
 docker rm thuis
 docker run \
         --detach \
+        --add-host 0828-4808-8456.local:192.168.1.14 \
         --restart unless-stopped \
         --publish 8088:8088 \
         --volume /opt/thuis/envdb.json:/usr/src/app/envdb.json \
